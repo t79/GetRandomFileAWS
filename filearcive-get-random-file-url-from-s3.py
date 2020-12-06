4ea87c280723c804457e9fcf579415fa1b52efaa
Terje Urnes <terje@urnes.net>
2020-12-06 23:59:34 +0100

    connecting to bucket

--- filearcive-get-random-file-url-from-s3.py
+++ filearcive-get-random-file-url-from-s3.py
@@ -1,17 +1,18 @@
 import json
 import boto3
 
+bucketName = "filearchive.t79.it"
 
 def lambda_handler(event, context):
     s3Client = boto3.client('s3')
 
     try:
-        bucketResponse = s3Client.list_buckets()
+        objectsResponse = s3Client.list_objects(Bucket=bucketName)
     except Exception as e:
         raise e
 
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! there are '
-                           + str(len(bucketResponse["Buckets"])) + ' bukets')
+                           + str(len(objectsResponse["Contents"])) + ' objects')
     }
