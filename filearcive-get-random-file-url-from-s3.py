b59293c7a5a0557ba8a81142642e6efb5063ed55
Terje Urnes <terje@urnes.net>
2020-12-07 00:00:55 +0100

    getting file names from bucket

--- filearcive-get-random-file-url-from-s3.py
+++ filearcive-get-random-file-url-from-s3.py
@@ -8,11 +8,17 @@ def lambda_handler(event, context):
 
     try:
         objectsResponse = s3Client.list_objects(Bucket=bucketName)
+
+        fileNamesList = []
+        for object in objectsResponse["Contents"]:
+            fileNamesList.append(object["Key"])
+
     except Exception as e:
         raise e
 
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda! there are '
-                           + str(len(objectsResponse["Contents"])) + ' objects')
+                           + str(len(fileNamesList)) + ' objects: '
+                           + " ".join(name for name in fileNamesList))
     }
